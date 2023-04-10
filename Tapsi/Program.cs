243cf7db7a0bbf4cc962ec4a7ffe16468df41ab0
behzad dara <Behzad.dara.99@gmail.com>
2023-04-10 20:04:20 +0430

    Add project files.

---
+++ Tapsi/Program.cs
@@ -0,0 +1,29 @@
+﻿
+var array = new int[] { 7, 3, 8, 2, 10, 10 };
+
+var score = new int[array.Length, array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    score[i,i] = array[i];
+}
+
+for (int j = 1; j < array.Length; j++)
+{
+    for (int i = 0; i < array.Length - j; i++)
+    {
+        if (j % 2 == 1)
+            score[i,i + j] = Math.Max(score[i + 1, i + j], score[i, i + j - 1] );
+        else
+            score[i,i + j] = Math.Min(score[i + 1,i + j] + array[i], score[i,i + j - 1] + array[i + j]);
+    }
+}
+
+for (int j = 0; j < array.Length; j++)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{score[i,j]}, ");
+    }
+    Console.WriteLine();
+}
